83b65ed5deffba66e93a111c49a6c8f9a7757baa
srinivasjlkm <srinivasjlkm@gmail.com>
2013-12-20 14:00:50 +0800

    New_version1.1

--- Assets/Scripts/rotatecam.js
+++
@@ -1,9 +0,0 @@
-﻿var speed : float = 5.0;
-var target : Transform;
-
-function Update () {
-    if (Input.GetMouseButton(1)) {
-        transform.LookAt(target);
-        transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X")*speed);
-    }
-}
